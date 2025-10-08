78e8a63889ffa4bce505247cf9892c3199c1b792
Erik <33792954+exliipso@users.noreply.github.com>
2025-10-08 01:20:10 -0500

    Add gradient support!
    - Adds gradient rendering to rounded shader: linear, radial, conic
    - New API: :GradientLinear, :GradientRadial, :GradientConic, :GradientTexture, :GradientTiling

--- src/rndx_rounded_psxx.hlsl
+++ src/rndx_rounded_psxx.hlsl
@@ -1,11 +1,35 @@
 #include "common_rounded.hlsl"
 
+
+float3 apply_gradient(PS_INPUT i, float3 base_rgb)
+{
+    if (GRAD_MODE < 0.5) {
+        return base_rgb;
+    }
+
+    float2 screen_pos = i.uv.xy * SIZE;
+    float2 rect_half_size = SIZE * 0.5;
+    float2 centered = gradient_centered_pos(screen_pos, rect_half_size);
+   
+    centered = rotate_point(centered);
+    float t = compute_gradient_t(centered);
+
+    if (GRAD_USE_RAMP > 0.5) {
+       
+        float4 ramp = tex2D(Tex2, float2(t, 0.5));
+        return ramp.rgb;
+    } else {
+     
+        return lerp(base_rgb * 0.0, base_rgb, t);
+    }
+}
+
 float4 main(PS_INPUT i) : COLOR {
     float alpha = calculate_rounded_alpha(i);
+    if (alpha <= 0.0f)
+        discard;
 
-	if (alpha <= 0.0f)
-		discard;
-
-    float4 rect_color = USE_TEXTURE == 1 ? tex2D(TexBase, i.uv.xy) * i.color : i.color;
-    return float4(rect_color.rgb, rect_color.a * alpha);
+    float4 col = (USE_TEXTURE == 1) ? tex2D(TexBase, i.uv.xy) * i.color : i.color;
+    float3 grad_rgb = apply_gradient(i, col.rgb);
+    return float4(grad_rgb, col.a * alpha);
 }
