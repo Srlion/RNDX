a7bd91bae405874cabc65ea0a4687d0cbe0e621d
darkfated <darkfated@icloud.com>
2025-10-10 22:35:59 +0300

    Replace discard

--- src/rndx_rounded_psxx.hlsl
+++ src/rndx_rounded_psxx.hlsl
@@ -3,8 +3,10 @@
 float4 main(PS_INPUT i) : COLOR {
     float alpha = calculate_rounded_alpha(i);
 
-	if (alpha <= 0.0f)
-		discard;
+    if (alpha <= 0.001f)
+    {
+        return float4(0.0, 0.0, 0.0, 0.0);
+    }
 
     float4 rect_color = USE_TEXTURE == 1 ? tex2D(TexBase, i.uv.xy) * i.color : i.color;
     return float4(rect_color.rgb, rect_color.a * alpha);
--- src/rndx_shadows_psxx.hlsl
+++ src/rndx_shadows_psxx.hlsl
@@ -3,9 +3,11 @@
 float4 main(PS_INPUT i) : COLOR {
     float alpha = calculate_smooth_rounded_alpha(i);
 
-	if (alpha <= 0.0f)
-		discard;
-		
+    if (alpha <= 0.001f)
+    {
+        return float4(0.0, 0.0, 0.0, 0.0);
+    }
+
     float4 rect_color = i.color;
     return float4(rect_color.rgb, rect_color.a * alpha);
 }
